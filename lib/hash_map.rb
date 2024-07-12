5cdd2480269fde9221a30697b52ec6bc979cd2b9
cereia <104402370+cereia@users.noreply.github.com>
2024-07-12 15:33:24 -0400

    Add hash method

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
+# a data structure that takes a key value pair, produces a hash code, and stores the pair in a bucket
 class HashMap
+  def initialize
+    @buckets = Array.new(16)
+    @load_factor = 0.75
+  end
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code
+  end
+
+  #   raise IndexError if index.negative? || index >= @buckets.length
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,18 @@
 # frozen_string_literal: true
 
 require_relative 'lib/hash_map'
+
+test = HashMap.new
+test.set('apple', 'red')
+# test.set('banana', 'yellow')
+# test.set('carrot', 'orange')
+# test.set('dog', 'brown')
+# test.set('elephant', 'gray')
+# test.set('frog', 'green')
+# test.set('grape', 'purple')
+# test.set('hat', 'black')
+# test.set('ice cream', 'white')
+# test.set('jacket', 'blue')
+# test.set('kite', 'pink')
+# test.set('lion', 'golden')
+puts test
